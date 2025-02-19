f5e06c6ffe547bb45ffcc0e089538a9653e6f41c
samaha74 <mm.boodysamaha@gmail.com>
2025-02-19 12:19:42 +0200

    done

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import Swal from './node_modules/sweetalert2/src/sweetalert2.js'
+
 let a= document.getElementById("A");
 let b= document.getElementById("B");
 let c= document.getElementById("C");
@@ -12,6 +14,8 @@ for(let i=1;i<=1000;++i)
         a.innerHTML=parseInt(a.innerHTML)+1;
         b.innerHTML=parseInt(b.innerHTML)+14;
         c.innerHTML=parseInt(c.innerHTML)+5;
+        if(i===1000)
+            Swal.fire("Welcom User");
     }
         , i*2
     )
